125aef71092747fc8af51214a255305d3c467c9b
Ashley Riehl and Shelby Clayton <shelby@>
2016-06-15 21:09:15 -0700

    functionality to our webpage.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,34 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var Items = ["item1", "item2", "item3", "item4", "item5"];
-    Items.forEach(function(Item) {
-      var userList = $("input#" + Item).val();
-      alert(userList);
+    var items = ["item1", "item2", "item3", "item4", "item5"];
+    var itemInputs = [];
+    items.forEach(function(item) {
+      var userList = $("input#" + item).val();
+      itemInputs.push(userList);
+    });
 
+    alert(itemInputs);
+
+
+      $("#finalList").show();
+      $("#blanks").hide();
+      event.preventDefault();
+      //return Items.toUpperCase();
+      //alert(userList);
+
+      $("ul").append("<li>" + itemInputs[0] + "</li>");
+      $("ul").append("<li>" + itemInputs[1] + "</li>");
+      $("ul").append("<li>" + itemInputs[2] + "</li>");
+      $("ul").append("<li>" + itemInputs[3] + "</li>");
+      $("ul").append("<li>" + itemInputs[4] + "</li>");
+       //$("." + Item).append(userList[0]);
     //var Lists = $("input#Items").val();
       //Items.forEach(function(Item) {
 
 
 
       //return Items.push(Item);
-     })
 
-    event.preventDefault();
+   });
+
   });
-});
